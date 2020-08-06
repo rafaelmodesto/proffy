0f0e1fd6d1a736f0d95ba048e4b16cfec537be03
rafaelmodesto <rafaelmodesto09@gmail.com>
2020-08-06 08:03:52 -0300

    Iniciando lista de professores

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -12,7 +13,7 @@ import './styles.css';
 function Landing() {
   return (
     <div id="page-landing">
-      <div id="page-landing" className="container">
+      <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
@@ -25,15 +26,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Estudar"/>
-            Estudar
-          </a>          
+            Dar aulas
+          </Link>          
         </div>
 
         <span className="total-connections">
